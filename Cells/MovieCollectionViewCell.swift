5f21308d4a82cea69c7b818cd9b90d53cffc7597
Gizem Kaya <gizemkaya17@ku.edu.tr>
2021-12-26 17:27:41 +0300

    Initial Commit

---
+++ Cells/MovieCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCollectionViewCell.swift
+//  RandomMovieSelector
+//
+//  Created by gizem.kaya on 26.12.2021.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var MovieLabel: UILabel!
+    @IBOutlet weak var MovieImageView: UIImageView!
+}
